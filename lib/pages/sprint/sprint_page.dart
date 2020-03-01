7ac62eee52f02d7a3b562d87a939ffe591ba09b4
Zach Taylor <zachtylr04@gmail.com>
2020-02-29 22:33:57 -0600

    refactor sprint page files

--- lib/pages/sprint/sprint_page.dart
+++ lib/pages/sprint/sprint_page.dart
@@ -1,12 +1,154 @@
 import 'package:flutter/cupertino.dart';
-import 'package:scatter/pages/sprint/sprint_timer.dart';
+import 'package:flutter/material.dart';
+import './sprint_timer.dart';
 
 
-class SprintPage extends StatelessWidget {
+class SprintPage extends StatefulWidget {
+  @override
+  _SprintPageState createState() => _SprintPageState();
+}
+
+class _SprintPageState extends State<SprintPage> with TickerProviderStateMixin {
+  AnimationController controller;
+  Duration timerDuration = Duration.zero;
+  bool started = false;
+  bool paused = false;
+  double value;
+
+  _onTimerDurationChanged(Duration duration) {
+    setState(() {
+      timerDuration = duration;
+      controller = AnimationController(
+        vsync: this,
+        duration: duration
+      )..addListener(() {
+        setState(() {
+          value = controller.value;
+        });
+      });
+    });
+  }
+
+  _onCancelPressed() {
+    setState(() {
+      controller.reset();
+      started = false;
+    });
+  }
+
+  _onStartPressed() {
+    setState(() {
+      started = true;
+    });
+    if (controller.isAnimating)
+      controller.stop();
+    else {
+      controller.reverse(
+        from: controller.value == 0.0
+          ? 1.0
+          : controller.value
+      );
+    }
+  }
+
+  String get timerString {
+    Duration duration = controller.duration * controller.value;
+    return duration.inHours.toString().padLeft(2, '0') + ':' +
+    (duration.inMinutes % 60).toString().padLeft(2, '0') + ':' + 
+    (duration.inSeconds % 60).toString().padLeft(2, '0');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 0)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SprintTimer()
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.center,
+                  child: AspectRatio(
+                    aspectRatio: 1.0,
+                    child: started ? Stack(
+                      children: <Widget>[
+                        Positioned.fill(
+                          child: AnimatedBuilder(
+                            animation: controller,
+                            builder: (context, child) {
+                              return CustomPaint(
+                                painter: SprintTimerPainter(
+                                  animation: controller,
+                                  backgroundColor: CupertinoColors.activeGreen,
+                                  color: CupertinoColors.black
+                                )
+                              );
+                            }
+                          )
+                        ),
+                        Align(
+                          alignment: FractionalOffset.center,
+                          child: AnimatedBuilder(
+                            animation: controller,
+                            builder: (context, child) {
+                              return Text(
+                                timerString,
+                                style: TextStyle(
+                                  fontSize: 80.0,
+                                  color: CupertinoColors.black
+                                )
+                              );
+                            }
+                          )
+                        )
+                      ],
+                    ) : CupertinoTimerPicker(
+                      initialTimerDuration: timerDuration,
+                      onTimerDurationChanged: _onTimerDurationChanged,
+                    )
+                  )
+                )
+              ),
+              AnimatedBuilder(
+                animation: controller,
+                builder: (context, child) {
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 100),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        CupertinoButton(
+                          child: Text('Cancel'),
+                          onPressed: _onCancelPressed
+                        ),
+                        CupertinoButton(
+                          child: Text(
+                            controller.isAnimating ? 'Pause' : 'Start'
+                          ),
+                          onPressed: _onStartPressed
+                        ),
+                      ]
+                     )
+                  );
+                }
+              )
+            ],
+          )
+        );
+      }
     );
   }
 }
+
--- lib/pages/sprint/sprint_timer.dart
+++ lib/pages/sprint/sprint_timer.dart
@@ -1,146 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'dart:math';
 
-
-class SprintTimer extends StatefulWidget {
-  @override
-  _SprintTimerState createState() => _SprintTimerState();
-}
-
-class _SprintTimerState extends State<SprintTimer> with TickerProviderStateMixin {
-  AnimationController controller;
-  Duration timerDuration = Duration.zero;
-  bool started = false;
-  bool paused = false;
-
-  _onTimerDurationChanged(Duration duration) {
-    setState(() {
-      timerDuration = duration;
-      controller = AnimationController(
-        vsync: this,
-        duration: duration
-      );
-    });
-  }
-
-  _onCancelPressed() {
-    setState(() {
-      controller.reset();
-      started = false;
-    });
-  }
-
-  _onStartPressed() {
-    setState(() {
-      started = true;
-    });
-    if (controller.isAnimating)
-      controller.stop();
-    else {
-      controller.reverse(
-        from: controller.value == 0.0
-          ? 1.0
-          : controller.value
-      );
-    }
-  }
-
-  String get timerString {
-    Duration duration = controller.duration * controller.value;
-    return duration.inHours.toString().padLeft(2, '0') + ':' +
-    (duration.inMinutes % 60).toString().padLeft(2, '0') + ':' + 
-    (duration.inSeconds % 60).toString().padLeft(2, '0');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (context, child) {
-        return Padding(
-          padding: EdgeInsets.all(25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(
-                child: Align(
-                  alignment: FractionalOffset.center,
-                  child: AspectRatio(
-                    aspectRatio: 1.0,
-                    child: started ? Stack(
-                      children: <Widget>[
-                        Positioned.fill(
-                          child: AnimatedBuilder(
-                            animation: controller,
-                            builder: (context, child) {
-                              return CustomPaint(
-                                painter: SprintTimerPainter(
-                                  animation: controller,
-                                  backgroundColor: CupertinoColors.activeGreen,
-                                  color: CupertinoColors.black
-                                )
-                              );
-                            }
-                          )
-                        ),
-                        Align(
-                          alignment: FractionalOffset.center,
-                          child: AnimatedBuilder(
-                            animation: controller,
-                            builder: (context, child) {
-                              return Text(
-                                timerString,
-                                style: TextStyle(
-                                  fontSize: 80.0,
-                                  color: CupertinoColors.black
-                                )
-                              );
-                            }
-                          )
-                        )
-                      ],
-                    ) : CupertinoTimerPicker(
-                      initialTimerDuration: timerDuration,
-                      onTimerDurationChanged: _onTimerDurationChanged,
-                    )
-                  )
-                )
-              ),
-              AnimatedBuilder(
-                animation: controller,
-                builder: (context, child) {
-                  return Padding(
-                    padding: EdgeInsets.only(bottom: 100),
-                    child: Row(
-                      children: <Widget>[
-                        CupertinoButton(
-                          child: Text('Cancel'),
-                          onPressed: _onCancelPressed
-                        ),
-                        CupertinoButton(
-                          child: Text(
-                            controller.isAnimating ? 'Pause' : 'Start'
-                          ),
-                          onPressed: _onStartPressed
-                        ),
-                      ]
-                     )
-                  );
-                }
-              )
-            ],
-          )
-        );
-      }
-    );
-  }
-}
 
 class SprintTimerPainter extends CustomPainter {
   final Animation<double> animation;
@@ -157,12 +17,12 @@ class SprintTimerPainter extends CustomPainter {
     Paint paint = Paint()
       ..color = backgroundColor
       ..strokeWidth = 10.0
-      ..strokeCap = StrokeCap.butt
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
     canvas.drawCircle(size.center(Offset.zero), size.width / 2.0, paint);
     paint.color = color;
-    double progress = (1.0 - animation.value) * 2 * pi;
-    canvas.drawArc(Offset.zero & size, pi * 1.5, -progress, false, paint);
+    double progress = (animation.value - 1.0) * 2 * pi;
+    canvas.drawArc(Offset.zero & size, pi * 1.5, progress, false, paint);
   }
   @override
   bool shouldRepaint(SprintTimerPainter oldDelegate) {
